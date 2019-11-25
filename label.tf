8fc7a0cf5004fce2ffb88a2a33071eba12f31afd
callumccr1 <callumccr@gmail.com>
2019-11-25 15:31:58 +1300

    development commit

--- label.tf
+++ label.tf
@@ -1,7 +1,6 @@
 module "dynamo_label" {
   source              = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.14.1"
   context             = var.context
-  regex_replace_chars = var.regex_replace_chars
   delimiter           = "-"
   attributes          = ["dyn"]
   additional_tag_map  = {} /* Additional attributes (e.g. 1) */
